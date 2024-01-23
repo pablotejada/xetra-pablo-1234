44fc3570320099a19a647724c5780c37442e96a2
pablotejada <pablo.tejada@atherasanalytics.com>
2024-01-23 16:55:08 +0000

    added tests

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,11 @@
+class MetaProcess():
+    """
+    Methods for processing the meta file
+    """
+
+    @staticmethod
+    def update_meta_file():
+        pass
+    @staticmethod
+    def return_date_list():
+        pass
